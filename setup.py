11eeb67183e159e807d645bdfcfd2d8c21f1b810
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-04-09 23:07:12 -0700

    adding basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ETAVTA-OS',
+      version='0.5.0',
+      description='ETAVTA (OpenShift variant)',
+      author='Mahmoud Hashemi',
+      author_email='example@example.com',
+      url='https://github.com/mahmoud/etavta-openshift',
+      install_requires=['clastic', 'mako'],
+      )
